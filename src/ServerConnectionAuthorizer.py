c06f74d69d1340e64570ae4f91d47083e6e2094e
Alexandru VIRVAREI <avirvarei@bitdefender.com>
2020-12-22 15:56:14 +0200

    refactoring

--- src/ServerConnectionAuthorizer.py
+++ src/ServerConnectionAuthorizer.py
@@ -5,10 +5,13 @@ import subprocess
 import DataSetParser
 import TransformersImplementation
 from logger.logger import LOGGER
-import os
 import traceback
+import os
+import numpy as np
+import tensorflow as tf
+import json
+import model, sample, encoder
 app = Flask(__name__)
-os.chdir("./gpt2")
 AUTHORIZATION_TOKEN_EXPECTED = 'xy124zjw3'
 
 def authorize(_function):
@@ -27,24 +30,62 @@ def authorize(_function):
 def processSentence(sentence):
 
     LOGGER.info("start processing sentence: {}".format(sentence))
-    #stemmer = TransformersImplementation.StemmerImplementation()
-    #lemmer = TransformersImplementation.LemmatizerImplementationSPACY()
-    #stop_words = TransformersImplementation.StopWordsImplementation()
-
-    #dataSetParser = DataSetParser.DataSetParser()
-    #dataSetParser.SetLemmatizer(lemmer)
-    #dataSetParser.SetStemmer(stemmer)
-    #dataSetParser.SetStopWords(stop_words)
-
-    #dataSetParser.PreProcessData(sentence)
-    #LOGGER.info("finished processing sentence: {}".format(sentence))
-    #LOGGER.info("New form of sentence: {}".format(dataSetParser.corpus))
-    #return dataSetParser.corpus
     try:
-        output = subprocess.check_output('python3 src/interactive_conditional_samples.py --raw_text "{}"'.format(sentence),shell=True)
-        response = output.decode('utf-8').split("\n")[-2]
-        print(response)
-        return response
+        # output = subprocess.check_output('python3 src/interactive_conditional_samples.py --raw_text "{}"'.format(sentence),shell=True)
+        # response = output.decode('utf-8').split("\n")[-2]
+        # print(response)
+        # return response
+        model_name = 'beta2'
+        seed = None
+        nsamples = 1
+        batch_size = 1
+        length = None
+        temperature = 1
+        top_k = 40
+        top_p = 0.0
+        raw_text = sentence
+
+        if batch_size is None:
+            batch_size = 1
+        assert nsamples % batch_size == 0
+
+        enc = encoder.get_encoder(model_name)
+        hparams = model.default_hparams()
+        with open(os.path.join('models', model_name, 'hparams.json')) as f:
+            hparams.override_from_dict(json.load(f))
+
+        if length is None:
+            length = hparams.n_ctx // 2
+        elif length > hparams.n_ctx:
+            raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+        with tf.Session(graph=tf.Graph()) as sess:
+            context = tf.placeholder(tf.int32, [batch_size, None])
+            np.random.seed(seed)
+            tf.set_random_seed(seed)
+            output = sample.sample_sequence(
+                hparams=hparams, length=length,
+                context=context,
+                batch_size=batch_size,
+                temperature=temperature, top_k=top_k, top_p=top_p
+            )
+
+            saver = tf.train.Saver()
+            ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+            saver.restore(sess, ckpt)
+
+            while True:
+                context_tokens = enc.encode(raw_text)
+                generated = 0
+                for _ in range(nsamples // batch_size):
+                    out = sess.run(output, feed_dict={
+                        context: [context_tokens for _ in range(batch_size)]
+                    })[:, len(context_tokens):]
+                    for i in range(batch_size):
+                        generated += 1
+                        text = enc.decode(out[i])
+                        return text.split('<|endoftext|>')[0]
+
     except:
         traceback.print_exc()
         exit()
